58b210ca23075ab984354ff932fc32b406c0b967
MaximPVladimirovich <maxim.bleeker@gmail.com>
2021-06-09 12:23:35 -0500

    Added controller file

--- server.js
+++ server.js
@@ -29,61 +29,6 @@ db.once(`open`, function () {
   console.log(`connected to Database`)
 })
 
-// Index
-app.get(`/balls`, (req, res) => {
-  Balls.find({}, function (error, balls) {
-    res.render(`index.ejs`, { balls })
-  })
-
-})
-// New
-app.get(`/balls/new`, (req, res) => {
-  res.render(`new.ejs`)
-
-})
-// Delete
-app.delete(`/balls/:id`, (req, res) => {
-  Balls.findByIdAndDelete(req.params.id, function (error, deletedBall) {
-    res.redirect(`/balls`)
-  })
-})
-
-// update 
-app.put(`/balls/:id`, (req, res) => {
-  const updatedBall = new Balls({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    origin: req.body.origin,
-    price: req.body.price,
-    inStock: req.body.inStock
-  })
-  Balls.updateOne({ _id: req.params.id }, updatedBall).then
-    (res.redirect(`/balls/${updatedBall._id}`))
-})
-
-// Create
-app.post(`/balls`, (req, res) => {
-  Balls.create(req.body, function (error, createdBall) {
-    res.redirect(`/balls`)
-  });
-});
-
-// -Edit
-app.get(`/balls/:id/edit`, (req, res) => {
-  Balls.findById(req.params.id, function (error, ball) {
-    res.render(`edit.ejs`, {
-      ball
-    })
-  })
-})
-
-// Show
-app.get(`/balls/:id`, (req, res) => {
-  Balls.findById(req.params.id, function (error, ball) {
-    res.render(`show.ejs`, { ball })
-  })
-})
 
 // Listen
 app.listen(port, function () {
